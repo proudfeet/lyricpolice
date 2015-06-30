8c327062551ead079b4f668cba9d058e518a523f
proudfeet <simon.d.proudfoot@gmail.com>
2015-06-30 19:24:15 -0400

    - create submission form - submitted lyrics are pushed up to Firebase DB

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(){
 	var db = new Firebase("https://lyric-police.firebaseio.com/");
 	var lyrics = db.child("lyrics");
-	lyrics.push({
-		lyric: "Getting stabbed in the back"
+
+	document.getElementById("lyric-submission-form").addEventListener("submit", function(ev){
+		ev.preventDefault();
+		submittedLyric = document.getElementById("offending-lyric").value;
+		lyrics.push({
+			lyric: submittedLyric
+		});
+		document.getElementById("lyric-submission-form").reset();
 	});
 });
