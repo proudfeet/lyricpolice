b2e8a2fe02e5d51d528bffe838ed9c39e77ecf2c
proudfeet <simon.d.proudfoot@gmail.com>
2015-07-02 21:29:53 -0400

    - list of lyrics from db now on page

--- script.js
+++ script.js
@@ -4,15 +4,18 @@ document.addEventListener("DOMContentLoaded", function(){
 
 	lyrics.on("value", function(snapshot){
 		// This function fires automatically if the DB has any values stored in it
-		var lyricList = Object(snapshot.val());
+		var dbLyricList = Object(snapshot.val());
+		var lyricList = "";
 		// lyric list is the Object form of the information in the DB
-		for(var key in lyricList) {
+		for(var key in dbLyricList) {
 			// checking for keys in the Object form of the DB
-			if(lyricList.hasOwnProperty(key)) {
+			if(dbLyricList.hasOwnProperty(key)) {
 				// checks whether or not the Object form of it's own, rather than just keys inherited due to prototyping
-				console.log(key + " => " + lyricList[key].lyric);
+				var lyric = "<li>" + dbLyricList[key].lyric + "</li>";
+				lyricList = lyricList + lyric;
 			}
 		}
+		document.getElementById("lyric-container").innerHTML = lyricList;
 	});
 
 	document.getElementById("lyric-submission-form").addEventListener("submit", function(ev){
